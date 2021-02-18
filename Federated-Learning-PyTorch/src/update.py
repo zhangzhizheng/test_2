2221e8c7c2811a4f89553334afeba134a68f8b36
xiyiyia <fuboqian@cug.edu.cn>
2021-02-18 23:00:24 +0900

    a

--- Federated-Learning-PyTorch/src/update.py
+++ Federated-Learning-PyTorch/src/update.py
@@ -124,6 +124,7 @@ def test_inference(args, model, test_dataset, groups):
     # testloader = DataLoader(test_dataset, batch_size=128,
     #                         shuffle=False)
     testloader = DataLoader(DatasetSplit(test_dataset, groups),batch_size=128,shuffle=False) # test non-IID
+    print(len(testloader))
     for _, (images, labels) in enumerate(testloader):
         images, labels = images.to(device), labels.to(device)
 
