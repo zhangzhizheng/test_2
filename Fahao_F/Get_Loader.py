004c20b57b20641373301376b8f1fdfb7c6d5593
xiyiyia <fuboqian@cug.edu.cn>
2021-02-21 16:05:16 +0900

    a

--- Fahao_F/Get_Loader.py
+++ Fahao_F/Get_Loader.py
@@ -62,10 +62,10 @@ class Get_Loader(object):
         #     print(labels[idx])
         #     time.sleep(1)
         if(self.args.data_distribution == 1):                    # Non-IID add
-            rand_set_all = [0, 20 ,60 ,80 , 100, 120]
-            # k = [10, 20, 10, 6,4, 2]
-            # rand_set_all = [0]
-            k = [20, 20, 20, 20,20, 20]
+            # rand_set_all = [0, 20 ,60 ,80 , 100, 120]
+            k = [125]
+            rand_set_all = [0]
+            # k = [20, 20, 20, 20,20, 20]
 
         if(self.args.data_distribution == 2):
             rand_set_all = [180, 160, 140 ,120, 100, 80]
@@ -118,14 +118,14 @@ class Get_Loader(object):
         idxs_labels = idxs_labels[:, idxs_labels[1, :].argsort()]
         idxs = idxs_labels[0, :]
 
-        rand_set_all_1 = [0, 10 ,20 ,30 ,40, 50 ]
-        rand_set_all_2 = [90, 80, 70, 60 ,50, 40]
-        # rand_set_all_1 = [0]
-        # rand_set_all_2 = [90]
+        # rand_set_all_1 = [0, 10 ,20 ,30 ,40, 50 ]
+        # rand_set_all_2 = [90, 80, 70, 60 ,50, 40]
+        rand_set_all_1 = [0]
+        rand_set_all_2 = [50]
         # k = [5, 10, 5, 3 ,2 ,1]
         # rand_set_all = {[0,90],[10,80],[20,70],[30,60],[40,50],[50,40],[60,30]}
-        #dis = [5, 10, 5, 3 ,2 ,1]
-        dis = [10, 10, 10, 10 ,10,10]
+        # dis = [5, 10, 5, 3 ,2 ,1]
+        dis = [49]
 
         for j in range(len(dis)):
             # print(rand_1, rand_2, j)
