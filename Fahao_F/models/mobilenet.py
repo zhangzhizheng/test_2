277154ebc7cc6d5d73e2477b9b343529889da331
xiyiyia <fuboqian@cug.edu.cn>
2021-02-22 19:22:39 +0900

    a

--- Fahao_F/models/mobilenet.py
+++ Fahao_F/models/mobilenet.py
@@ -12,7 +12,7 @@ class Block(nn.Module):
     '''Depthwise conv + Pointwise conv'''
     def __init__(self, in_planes, out_planes, stride=1):
         super(Block, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, in_planes, kernel_size=3, stride=stride, padding=1, groups=in_planes, bias=False)
+        self.conv1 = nn.Conv2d(in_planes, in_planes, kernel_size=2, stride=2, padding=1, groups=in_planes, bias=False)
         self.bn1 = nn.BatchNorm2d(in_planes)
         self.conv2 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn2 = nn.BatchNorm2d(out_planes)
