2d93555ff10b408ee80027ef055c18a6481c6a47
xiyiyia <fuboqian@cug.edu.cn>
2021-02-22 13:13:48 +0900

    a

--- Fahao_F/models/mobilenet_m2.py
+++ Fahao_F/models/mobilenet_m2.py
@@ -19,7 +19,7 @@ class VGG(nn.Module):
         self.classifier = nn.Linear(512, 10)
 
     def forward(self, x):
-        print("zhendejiade?")
+        # print("zhendejiade?")
         out = self.features(x)
         out = out.view(out.size(0), -1)
         out = self.classifier(out)
