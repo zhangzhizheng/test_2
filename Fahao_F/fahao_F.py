36e662d283cb3e890df50828cd57037c042570c9
xiyiyia <fuboqian@cug.edu.cn>
2021-02-22 11:07:51 +0900

    A

--- Fahao_F/fahao_F.py
+++ Fahao_F/fahao_F.py
@@ -137,6 +137,13 @@ def Set_model(net, client, args):
                         momentum=0.9, weight_decay=5e-4)
         global_model = MobileNetV2()
         return Model, global_model, Optimizer
+    elif net == 'vgg':
+        for i in range (client):
+            Model[i] = VGG('VGG19')
+            Optimizer[i] = torch.optim.SGD(Model[i].parameters(), lr=args.lr,
+                        momentum=0.9, weight_decay=5e-4)
+        global_model = VGG('VGG19')
+        return Model, global_model, Optimizer
     elif net == 'MobileNetM2':
         for i in range (client):
             Model[i] = MobileNetM2()
