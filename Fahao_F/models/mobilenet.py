906372c19a2786cbf3580069924fd36fac724296
xiyiyia <fuboqian@cug.edu.cn>
2021-02-24 22:34:53 +0900

    a

--- Fahao_F/models/mobilenet.py
+++ Fahao_F/models/mobilenet.py
@@ -12,12 +12,12 @@ class Block(nn.Module):
     '''Depthwise conv + Pointwise conv'''
     def __init__(self, in_planes, out_planes, stride):
         super(Block, self).__init__()
-        if(stride == 1):
+        if(stride == 1 or stride == 3):
             if(in_planes == out_planes):
-                self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, padding=0, bias=False)
+                self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=1, padding=0, bias=False)
                 self.bn1 = nn.BatchNorm2d(out_planes)
             elif(in_planes != out_planes):
-                self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=0, bias=False)
+                self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, padding=0, bias=False)
                 self.bn1 = nn.BatchNorm2d(out_planes)              
         elif(stride == 2):
             self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
@@ -34,7 +34,7 @@ class Block(nn.Module):
 
 class MobileNet(nn.Module):
     # (128,2) means conv planes=128, conv stride=2, by default conv stride=1
-    cfg = [64, (128,2), 128, (256,2), 256, (512,2), 512, 512, 512, 512, 512, (1024,2), 1024]
+    cfg = [64, (64,2),128, (128,2), 256, (256,2),   512, 512, 512, 512, 512, (512,2), (1024,3), 1024]
 
     def __init__(self, num_classes=10):
         super(MobileNet, self).__init__()
