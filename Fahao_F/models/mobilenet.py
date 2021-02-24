0c98bb6b66b0d34a3f65a0b2c52e8d42dbd8b95f
xiyiyia <fuboqian@cug.edu.cn>
2021-02-25 08:52:52 +0900

    a

--- Fahao_F/models/mobilenet.py
+++ Fahao_F/models/mobilenet.py
@@ -10,41 +10,29 @@ import torch.nn.functional as F
 
 class Block(nn.Module):
     '''Depthwise conv + Pointwise conv'''
-    def __init__(self, in_planes, out_planes, stride):
+    def __init__(self, in_planes, out_planes, stride=1):
         super(Block, self).__init__()
-        if(stride == 1 or stride == 3):
-            if(in_planes == out_planes):
-                self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=stride, stride=1, padding=1, bias=False)
-                self.bn1 = nn.BatchNorm2d(out_planes)
-            elif(in_planes != out_planes):
-                self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, padding=0, bias=False)
-                self.bn1 = nn.BatchNorm2d(out_planes)              
-        elif(stride == 2):
-            # print(1)
-            self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
-            self.bn1 = nn.BatchNorm2d(out_planes)
-        # self.dropout_1 = nn.Dropout(0.2)
-        # self.dropout_2 = nn.Dropout(0.5)
+        self.conv1 = nn.Conv2d(in_planes, in_planes, kernel_size=3, stride=stride, padding=1, groups=in_planes, bias=False)
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv2 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_planes)
 
     def forward(self, x):
-        print(x.shape)
         out = F.relu(self.bn1(self.conv1(x)))
-        # out = F.relu(self.bn2(self.conv2(out)))
-        # out = self.dropout_1(out)
+        out = F.relu(self.bn2(self.conv2(out)))
         return out
 
 
 class MobileNet(nn.Module):
     # (128,2) means conv planes=128, conv stride=2, by default conv stride=1
-    cfg = [64, (64,2),128, (128,2), 256, (256,2),   512, 512, 512, 512, 512, (512,2), 1024, 1024]
+    cfg = [64, (128,2), 128, (256,2), 256, (512,2), 512, 512, 512, 512, 512, (1024,2), 1024]
 
     def __init__(self, num_classes=10):
         super(MobileNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(32)
         self.layers = self._make_layers(in_planes=32)
         self.linear = nn.Linear(1024, num_classes)
-        self.dropout = nn.Dropout(0.25)
 
     def _make_layers(self, in_planes):
         layers = []
@@ -60,7 +48,6 @@ class MobileNet(nn.Module):
         out = self.layers(out)
         out = F.avg_pool2d(out, 2)
         out = out.view(out.size(0), -1)
-        out = self.dropout(out)
         out = self.linear(out)
         return out
 
