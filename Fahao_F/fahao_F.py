a492a3f7eda71ffd0d037513d46a7a81e433ac44
xiyiyia <fuboqian@cug.edu.cn>
2021-02-25 15:44:14 +0900

    a

--- Fahao_F/fahao_F.py
+++ Fahao_F/fahao_F.py
@@ -91,13 +91,13 @@ def Set_dataset(dataset):
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.5, 0.5, 0.5)),
+            transforms.Normalize((0.5), (0.5)),
         ])
 
         transform_test = transforms.Compose([
             transforms.ToTensor(),
             # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.5, 0.5, 0.5)),
+            transforms.Normalize((0.5), (0.5)),
         ])
 
         trainset = torchvision.datasets.MNIST(root='/home/test_2/mnist/', train=True, download=True, transform=transform_train)
