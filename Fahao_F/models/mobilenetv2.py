fe0cb61b21fd04f9b09c0676cd1402d325eba312
xiyiyia <fuboqian@cug.edu.cn>
2021-02-25 16:21:04 +0900

    a

--- Fahao_F/models/mobilenetv2.py
+++ Fahao_F/models/mobilenetv2.py
@@ -29,7 +29,7 @@ class MobileNetV2(nn.Module):
 
     def __init__(self, num_classes=10):
         super(MobileNetV2, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(32)
         self.layers = self._make_layers(in_planes=32)
         self.linear = nn.Linear(1024, num_classes)
