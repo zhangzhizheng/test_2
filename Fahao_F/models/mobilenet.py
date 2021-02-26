448d996601acb7fe2e0a896551da1f5b2568c07d
xiyiyia <fuboqian@cug.edu.cn>
2021-02-27 03:38:36 +0900

    a

--- Fahao_F/models/mobilenet.py
+++ Fahao_F/models/mobilenet.py
@@ -12,15 +12,14 @@ class Block(nn.Module):
     '''Depthwise conv + Pointwise conv'''
     def __init__(self, in_planes, out_planes, stride):
         super(Block, self).__init__()
-        if(stride == 1 or stride == 3):
-            if(in_planes == out_planes):
-                self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=stride, stride=1, padding=0, bias=False)
-                self.bn1 = nn.BatchNorm2d(out_planes)
-            elif(in_planes != out_planes):
-                self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, padding=0, bias=False)
-                self.bn1 = nn.BatchNorm2d(out_planes)              
+        if(stride == 1):
+                self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1, padding=0, bias=False)
+                self.bn1 = nn.BatchNorm2d(out_planes)            
         elif(stride == 2):
             # print(1)
+            self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+            self.bn1 = nn.BatchNorm2d(out_planes)
+        elif(stride == 4):
             self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
             self.bn1 = nn.BatchNorm2d(out_planes)
         # self.dropout_1 = nn.Dropout(0.2)
@@ -36,7 +35,7 @@ class Block(nn.Module):
 
 class MobileNet(nn.Module):
     # (128,2) means conv planes=128, conv stride=2, by default conv stride=1
-    cfg = [64, (64,2),128, (128,2), 256, (256,2),   512] # , 512, 512, 512, 512, 512, 1024, 1024]
+    cfg = [64, (64,2), 128, 256, (512,4)] # , 512, 512, 512, 512, 512, 1024, 1024]
 
     def __init__(self, num_classes=10):
         super(MobileNet, self).__init__()
