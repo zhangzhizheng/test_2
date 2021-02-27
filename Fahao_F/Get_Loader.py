5c67a530ffdac017f1b406b6f7bbbcef0742cf6f
xiyiyia <fuboqian@cug.edu.cn>
2021-02-28 05:02:24 +0900

    a

--- Fahao_F/Get_Loader.py
+++ Fahao_F/Get_Loader.py
@@ -276,7 +276,7 @@ def load_databatch(data_folder, idx, img_size=64):
     dic_data = {}
     for i in range(1,11):
         fh = open(data_file + str(i), 'rb')
-        del fh['mean']
+        fh.pop('mean')
         dic_data.update(fh)
     dic_data = dic_data.fromkeys(dic_data['labels'], dic_data['data'])
     print(dic_data)
