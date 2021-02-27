f46536bcf473ba946b3108716684d2786a27156a
xiyiyia <fuboqian@cug.edu.cn>
2021-02-28 06:16:52 +0900

    a

--- Fahao_F/fahao_F.py
+++ Fahao_F/fahao_F.py
@@ -269,10 +269,9 @@ def Set_dataset(dataset):
             
             # transforms.Resize((32,32)),
             # transforms.RandomHorizontalFlip(),
-            
+            transforms.ToTensor(),
             # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.5, 0.5, 0.5)),
-            transforms.ToTensor(),
         ])
 
         transform_test = transforms.Compose([
