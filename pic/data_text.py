fd6f9be2d4ffc6dbaf44bf299603cf2675433b7f
xiyiyia <fuboqian@cug.edu.cn>
2021-02-28 16:54:54 +0900

    a

---
+++ pic/data_text.py
@@ -0,0 +1,8 @@
+import os
+file_name_caltech = '/home/caltech/data_list'
+file_name_animals = '/home/animals/data_list'
+
+caltech = os.listdir(file_name_caltech)
+animals = os.listdir(file_name_animals)
+# with open(file_name_101, 'a') as caltech:
+# with open(file_name_animals, 'a') as animals:
