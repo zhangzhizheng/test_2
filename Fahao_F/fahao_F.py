74b61df0aadde01a7f086331133cab69f92d3bc9
xiyiyia <fuboqian@cug.edu.cn>
2021-02-28 20:15:07 +0900

    a

--- Fahao_F/fahao_F.py
+++ Fahao_F/fahao_F.py
@@ -181,7 +181,8 @@ def Set_dataset(dataset):
         # ['panda','dogs', 'cats']
         print('==> Preparing data..')
         transform_train = transforms.Compose([
-            transforms.RandomCrop(32, padding=4),
+            # transforms.RandomCrop(32, padding=4),
+            transforms.Resize((32,32)),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
