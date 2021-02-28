4843b5d3c0fe762b9a38ad6588dc6267d507f509
xiyiyia <fuboqian@cug.edu.cn>
2021-02-28 17:04:44 +0900

    a

--- pic/data_text.py
+++ pic/data_text.py
@@ -1,9 +1,20 @@
 import os
 file_name_caltech = '/home/caltech/data_list'
 file_name_animals = '/home/animals/data_list'
-
-caltech = os.listdir('/home/caltech/')
-animals = os.listdir('/home/animals/')
-print(caltech, animals)
-# with open(file_name_101, 'a') as caltech:
-# with open(file_name_animals, 'a') as animals:
+caltech_class = os.listdir('/home/caltech/')
+animals_class = os.listdir('/home/animals/')
+#print(caltech, animals)
+with open(file_name_caltech, 'a') as caltech:
+    k = 0
+    for i in caltech_class:
+        images = os.listdir('/home/caltech/' + i)
+        for j in images:
+            caltech.write(j,k)
+        k += 1
+with open(file_name_animals, 'a') as animals:
+    k = 0
+    for i in animals_class:
+        images = os.listdir('/home/animals/' + i)
+        for j in images:
+            animals.write(j,k)
+        k += 1
