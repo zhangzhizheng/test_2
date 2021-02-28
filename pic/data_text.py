7e2d186f41e52f1d30b629c90e08ef8b98a5d5ae
xiyiyia <fuboqian@cug.edu.cn>
2021-02-28 16:56:14 +0900

    a

--- pic/data_text.py
+++ pic/data_text.py
@@ -2,7 +2,7 @@ import os
 file_name_caltech = '/home/caltech/data_list'
 file_name_animals = '/home/animals/data_list'
 
-caltech = os.listdir(file_name_caltech)
-animals = os.listdir(file_name_animals)
+caltech = os.listdir('/home/caltech/')
+animals = os.listdir('/home/animals/')
 # with open(file_name_101, 'a') as caltech:
 # with open(file_name_animals, 'a') as animals:
