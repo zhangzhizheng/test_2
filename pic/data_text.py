c035581a2be2ebde546f52996fbb4ec0410fcb8e
xiyiyia <fuboqian@cug.edu.cn>
2021-02-28 16:57:07 +0900

    a

--- pic/data_text.py
+++ pic/data_text.py
@@ -4,5 +4,6 @@ file_name_animals = '/home/animals/data_list'
 
 caltech = os.listdir('/home/caltech/')
 animals = os.listdir('/home/animals/')
+print(caltech, animals)
 # with open(file_name_101, 'a') as caltech:
 # with open(file_name_animals, 'a') as animals:
