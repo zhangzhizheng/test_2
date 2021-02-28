aa0d6749f766ffbdd4134872c125669b25923b43
xiyiyia <fuboqian@cug.edu.cn>
2021-02-28 23:16:43 +0900

    a

--- Fahao_F/models/vgg.py
+++ Fahao_F/models/vgg.py
@@ -15,7 +15,7 @@ class VGG(nn.Module):
     def __init__(self, vgg_name):
         super(VGG, self).__init__()
         self.features = self._make_layers(cfg[vgg_name])
-        self.classifier = nn.Linear(512, 3)
+        self.classifier = nn.Linear(512, 6)
 
     def forward(self, x):
         out = self.features(x)
