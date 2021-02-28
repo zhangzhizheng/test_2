157146157a77e3bcddee1f64a8771c6786de66b4
xiyiyia <fuboqian@cug.edu.cn>
2021-02-28 21:34:48 +0900

    a

--- Fahao_F/models/mobilenet.py
+++ Fahao_F/models/mobilenet.py
@@ -38,12 +38,12 @@ class MobileNet(nn.Module):
     # (128,2) means conv planes=128, conv stride=2, by default conv stride=1
     cfg = [64, (64,2),128, (128,2), 256, (256,2),   512] # , 512, 512, 512, 512, 512, 1024, 1024]
 
-    def __init__(self, num_classes=3):
+    def __init__(self, num_classes=10):
         super(MobileNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(32)
         self.layers = self._make_layers(in_planes=32)
-        self.linear = nn.Linear(512, 3)
+        self.linear = nn.Linear(512, 10)
         self.dropout = nn.Dropout(0.25)
 
     def _make_layers(self, in_planes):
