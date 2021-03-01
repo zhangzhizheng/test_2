8e67a7f149d8fe12b64919b2d7e349f4433836b6
xiyiyia <fuboqian@cug.edu.cn>
2021-03-01 14:03:36 +0900

    a

--- Fahao_F/models/mobilenet_m2.py
+++ Fahao_F/models/mobilenet_m2.py
@@ -16,7 +16,7 @@ class VGG(nn.Module):
     def __init__(self, vgg_name):
         super(VGG, self).__init__()
         self.features = self._make_layers(cfg[vgg_name])
-        self.classifier = nn.Linear(512, 80)
+        self.classifier = nn.Linear(512, 100)
         self.dropout = nn.Dropout(0.5)
     def forward(self, x):
         # print("zhendejiade?")
