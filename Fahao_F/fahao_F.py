a7d657ef2e63f185399aa3fdfdfab7c388bf0658
xiyiyia <fuboqian@cug.edu.cn>
2021-03-02 12:54:22 +0900

    a

--- Fahao_F/fahao_F.py
+++ Fahao_F/fahao_F.py
@@ -28,7 +28,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def Set_dataset(dataset):
     # print(dataset)
-    if dataset == 'CIFAR100':
+    if dataset == 'CIFAR10':
         # parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
         # parser.add_argument('--lr', default=0.01, type=float, help='learning rate')
         # parser.add_argument('--resume', '-r', action='store_true',
@@ -52,7 +52,7 @@ def Set_dataset(dataset):
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.5, 0.5, 0.5)),
         ])
 
-        trainset = torchvision.datasets.CIFAR100(root='/home/test_2/cifar-10-batches-py/', train=True, download=True, transform=transform_train)
+        trainset = torchvision.datasets.CIFAR10(root='/home/test_2/cifar-10-batches-py/', train=True, download=True, transform=transform_train)
         for i in range(args.num_users):
             train_class = Get_Loader(args, trainset, i+1)
             # print(train_class)
@@ -60,7 +60,7 @@ def Set_dataset(dataset):
             # trainloader = torch.utils.data.DataLoader(
             #     trainset, batch_size=128, shuffle=True, num_workers=2)
 
-        testset = torchvision.datasets.CIFAR100( root='/home/test_2/cifar-10-batches-py/', train=False, download=True, transform=transform_test)
+        testset = torchvision.datasets.CIFAR10( root='/home/test_2/cifar-10-batches-py/', train=False, download=True, transform=transform_test)
         test_class = Get_Loader(args, testset, 1)
         if(args.iid == 1):
             testloader_d1, testloader_d2 = test_class.get_test_dataloader_niid(testset)
