409bea5d18d971b6ac8c7263ba7dd5c3d8d99aa3
xiyiyia <fuboqian@cug.edu.cn>
2021-03-02 10:54:18 +0900

    a

--- Fahao_F/Get_Loader.py
+++ Fahao_F/Get_Loader.py
@@ -69,7 +69,7 @@ class Get_Loader(object):
         labels_list = {i: np.array([]) for i in range(0,10)}
         # labels_list = []
         for i in idxs:
-            labels_list[labels[i]] = np.append((labels_list[labels[i]],i))
+            labels_list[labels[i]] = np.append(labels_list[labels[i]],i)
         print(len(labels_list))
         print(labels_list)
         if(self.args.data_distribution == 1):                    # Non-IID add
