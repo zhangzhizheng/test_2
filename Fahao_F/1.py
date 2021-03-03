71f0a0c2a43cf23ea725d613d17dd888257a777c
xiyiyia <fuboqian@cug.edu.cn>
2021-03-03 20:56:47 +0900

    a

--- Fahao_F/1.py
+++ Fahao_F/1.py
@@ -1,18 +1,27 @@
-import os
-import pandas as pd
+# import os
+# import pandas as pd
 
-filePath = '/home/animals/'
-list = ['panda','dogs','cats']
-file_name_list = []
-iamge_label = []
-k = 0
-for i in list:
-        file_name_list.append(os.listdir(filePath + i))
-        for j in range(len(os.listdir(filePath + i))):
-                iamge_label.append(k)
-        k += 1
-file_name_list = eval('[%s]'%repr(file_name_list).replace('[', '').replace(']', ''))
-print(len(file_name_list))
-print(len(iamge_label))
-dataframe = pd.DataFrame({'FileName':file_name_list,',Labels':iamge_label})
-dataframe.to_csv("/home/animals/labels.csv",index=False,sep=',')
+# filePath = '/home/animals/'
+# list = ['panda','dogs','cats']
+# file_name_list = []
+# iamge_label = []
+# k = 0
+# for i in list:
+#         file_name_list.append(os.listdir(filePath + i))
+#         for j in range(len(os.listdir(filePath + i))):
+#                 iamge_label.append(k)
+#         k += 1
+# file_name_list = eval('[%s]'%repr(file_name_list).replace('[', '').replace(']', ''))
+# print(len(file_name_list))
+# print(len(iamge_label))
+# dataframe = pd.DataFrame({'FileName':file_name_list,',Labels':iamge_label})
+# dataframe.to_csv("/home/animals/labels.csv",index=False,sep=',')
+
+from autodl.convertor.image_to_tfrecords import autoimage_2_autodl_format
+
+def convertor_image_demo():
+    str = '/home'
+    raw_autoimage_datadir = f"/home/animals/"
+    autoimage_2_autodl_format(input_dir=raw_autoimage_datadir)
+
+convertor_image_demo()
