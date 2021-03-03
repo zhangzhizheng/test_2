ed68f7370169428d8f9c102f4e74a9bd83b57db4
xiyiyia <fuboqian@cug.edu.cn>
2021-03-03 20:25:07 +0900

    a

--- Fahao_F/1.py
+++ Fahao_F/1.py
@@ -1,4 +1,12 @@
 import os
 
-filePath = '/home/animals'
-print(os.listdir(filePath))
+filePath = '/home/animals/'
+list = ['panda','dogs','cats']
+file_name_list = []
+iamge_label = []
+k = 0
+for i in list:
+        file_name_list.append(os.listdir(filePath + i))
+        for j in len(os.listdir(filePath + i)):
+                iamge_label.append(k)
+        k += 1
