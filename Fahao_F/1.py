0a0c94591afdfa46f4117f13b2d3284319091453
xiyiyia <fuboqian@cug.edu.cn>
2021-03-04 09:00:26 +0900

    a

--- Fahao_F/1.py
+++ Fahao_F/1.py
@@ -1,21 +1,58 @@
-import model_search
-from model_search import constants
-from model_search import single_trainer
-from model_search.data import csv_data
+import os
+import time
+import argparse
 
-trainer = single_trainer.SingleTrainer(
-    data=csv_data.Provider(
-        label_index=0,
-        logits_dimension=2,
-        record_defaults=[0, 0, 0, 0],
-        filename="model_search/data/testdata/csv_random_data.csv"),
-    spec=constants.DEFAULT_DNN)
+from autodl.auto_ingestion import data_io
+from autodl.utils.util import get_solution
+from autodl.metrics import autodl_auc
+from autodl.auto_ingestion.dataset import AutoDLDataset
+from autodl.auto_models.auto_image.model import Model as ImageModel
 
-trainer.try_models(
-    number_models=200,
-    train_steps=1000,
-    eval_steps=100,
-    root_dir="/tmp/run_example",
-    batch_size=32,
-    experiment_name="example",
-    experiment_owner="model_search_user")
+from autodl import AutoDLDataset
+from autodl.utils.util import get_solution
+from autodl.metrics import autodl_auc, accuracy
+
+
+def run_single_model(model, dataset_dir, basename, time_budget=1200, max_epoch=50):
+    D_train = AutoDLDataset(os.path.join(dataset_dir, basename, "train"))
+    D_test = AutoDLDataset(os.path.join(dataset_dir, basename, "test"))
+    solution = get_solution(solution_dir=dataset_dir)
+
+    start_time = int(time.time())
+    for i in range(max_epoch):
+        remaining_time_budget = start_time + time_budget - int(time.time())
+        model.fit(D_train.get_dataset(), remaining_time_budget=remaining_time_budget)
+
+        remaining_time_budget = start_time + time_budget - int(time.time())
+        y_pred = model.predict(D_test.get_dataset(), remaining_time_budget=remaining_time_budget)
+
+        # Evaluation.
+        nauc_score = autodl_auc(solution=solution, prediction=y_pred)
+        acc_score = accuracy(solution=solution, prediction=y_pred)
+
+        print("Epoch={}, evaluation: nauc_score={}, acc_score={}".format(i, nauc_score, acc_score))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="tabular example arguments")
+    parser.add_argument("--input_data_path", type=str, help="path of input data")
+    args = parser.parse_args()
+
+    input_dir = os.path.dirname(args.input_data_path)
+
+    autoimage_2_autodl_format(input_dir=input_dir)
+
+    new_dataset_dir = input_dir + "_formatted" + "/" + os.path.basename(input_dir)
+    datanames = data_io.inventory_data(new_dataset_dir)
+    basename = datanames[0]
+    print("train_path: ", os.path.join(new_dataset_dir, basename, "train"))
+
+    D_train = AutoDLDataset(os.path.join(new_dataset_dir, basename, "train"))
+    D_test = AutoDLDataset(os.path.join(new_dataset_dir, basename, "test"))
+
+    max_epoch = 50
+    time_budget = 1200
+
+    model = ImageModel(D_train.get_metadata())
+
+    run_single_model(model, new_dataset_dir, basename, time_budget, max_epoch)
--- Fahao_F/autosklearn/classification.py
+++
@@ -1,1 +0,0 @@
-from autosklearn.estimators import AutoSklearnClassifier  # noqa (imported but unused)
