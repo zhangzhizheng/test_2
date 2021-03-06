b65945ebea79dd7393de68b5624a209a38684def
xiyiyia <fuboqian@cug.edu.cn>
2021-03-06 14:56:41 +0900

    a

--- Fahao_F/train_milenas.py
+++ Fahao_F/train_milenas.py
@@ -41,7 +41,7 @@ parser.add_argument('--init_channels', type=int, default=16, help='num of init c
 parser.add_argument('--layers', type=int, default=8, help='total number of layers')
 parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
 parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
-parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
+parser.add_argument('--cutout_length', type=int, default=8, help='cutout length')
 parser.add_argument('--drop_path_prob', type=float, default=0.3, help='drop path probability')
 parser.add_argument('--save', type=str, default='EXP', help='experiment name')
 parser.add_argument('--seed', type=int, default=2, help='random seed')
