35ad1a5af1683837519fa2146ae850ea99c04df8
xiyiyia <fuboqian@cug.edu.cn>
2021-03-07 21:02:29 +0900

    a

--- Fahao_F/fahao_F.py
+++ Fahao_F/fahao_F.py
@@ -489,7 +489,7 @@ def run(dataset, client, args):
 
     # file_name = '/home/test_2/cifar-gcn-drl/{}_{}_{}_{}_{}.pkl'.format(args.data_distribution, 
     # args.iid, args.epoch, args.net, args.dataset) # 4 layer
-    file_name = '/home/test_2/cifar-gcn-drl/5(2)_layer_0.pkl'
+    file_name = '/home/test_2/cifar-gcn-drl/5(2)_layer_{}}.pkl'.format(args.data_distribution)
     with open(file_name, 'wb') as f:
         pickle.dump([acc_list, loss_list], f)
             # pickle.dump([acc_list_1, loss_list_1], f)
