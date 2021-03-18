280ceb68561da61d1e7eec56ac986e965d677a8f
xiyiyia <fuboqian@cug.edu.cn>
2021-03-18 20:22:48 +0900

    a

--- Fahao_F/fahao_F.py
+++ Fahao_F/fahao_F.py
@@ -391,7 +391,7 @@ def Train(model, optimizer, client, trainloader):
             total[i] += targets.size(0)
             correct[i] += predicted.eq(targets).sum().item()
         print(train_loss[i] / len(trainloader[i])) # average over number of mini-batch
-        print(correct[i] / len(trainloader.dataset))
+        print(correct[i] / len(trainloader[i].dataset))
     time_end = time.time()
     if device == 'cuda':
         for i in range (client):
