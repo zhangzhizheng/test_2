fddce02d874d7abb58ccee196e0be89520036ba8
xiyiyia <fuboqian@cug.edu.cn>
2021-03-18 19:39:42 +0900

    a

--- Fahao_F/fahao_F.py
+++ Fahao_F/fahao_F.py
@@ -371,8 +371,7 @@ def Train(model, optimizer, client, trainloader):
     Loss = [0 for i in range (client)]
     time_start = time.time()
     for i in range(0,client):
-        print(i)
-        print(trainloader[i])
+        print(trainloader[i][0])
         for batch_idx, (inputs, targets) in trainloader[i]:
             # for i in targets:
             #     labels_check[i] += 1
