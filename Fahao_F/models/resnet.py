b09541b1560092011e5fe11e4785746d595ae24c
xiyiyia <fuboqian@cug.edu.cn>
2021-06-02 12:05:47 +0900

    a

--- Fahao_F/models/resnet.py
+++ Fahao_F/models/resnet.py
@@ -39,8 +39,8 @@ class BasicBlock(nn.Module):
         conv_1 = self.conv1(x)
         time_stop = time.time()
         #print('convo_1_'+str(self.in_planes/64)+':', time_stop-time_start)
-        # if(self.in_planes/64 == 8): 
-        #     print(self.a)
+        if(planes == 512): 
+            print(self.a)
         with open('/home/test_2/time/convo_1_'+str(self.in_planes/64)+'_'+str(self.a)+'.pkl', 'wb') as f:
             #print('a')
             pickle.dump(conv_1, f)
